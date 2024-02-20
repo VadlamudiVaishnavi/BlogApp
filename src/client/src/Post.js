25292422b6ca268330a0cf228d2d9ffcf89746d5
VadlamudiVaishnavi <vaishv@bu.edu>
2024-02-19 21:12:31 -0500

    Day 1, Main page

---
+++ src/client/src/Post.js
@@ -0,0 +1,18 @@
+export default function post(){
+    return(
+        <div className="post">
+            <div className="image">
+                <img src="cat.jpg" alt="cat"/>
+            </div>
+            <div className="texts">
+                <h2>Live your nine lives!</h2>
+                <p className="info">
+                  <a className="author">Vaishnavi Vadlamudi</a>
+                  <time>02/20/2024 16:45</time>
+                </p>
+                <p className="summary">In the quaint village of Eldoria, nestled between whispering forests and serene hills, there lived a young girl named Lila. Lila was known for her boundless curiosity and a heart as wide as the horizon. Unlike the other villagers, who were content with their daily routines, Lila yearned for adventure and dreamt of discovering what lay beyond the familiar landscapes of Eldoria.</p>
+            </div>
+        </div>
+    );
+}
+
